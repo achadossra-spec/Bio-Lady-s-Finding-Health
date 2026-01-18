71dc3bcf0481d86cf8e7efade7470b1e53594ed9
achadossra-spec <achadossra@gmail.com>
2026-01-18 03:37:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function redirectToStore() {
+    const go = confirm("Você será redirecionado para a loja. Deseja continuar?");
+    if (go) {
+        window.location.href = "https://www.digistore24.com/redir/568774/AntonioGlauber/";
+    }
+}
